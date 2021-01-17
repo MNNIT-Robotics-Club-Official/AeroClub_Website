7d19fa5be6ff269fa2df5c9e0343ec866fc3cbb0
me-abhinav-1001 <65178804+me-abhinav-1001@users.noreply.github.com>
2021-01-17 12:32:13 +0530

    added dummy text, and tweaked margin

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -7,12 +7,12 @@ export default function Footer() {
       {/* Site footer */}
       <footer className="site-footer container-fluid">
         <div className="topBottomMargin">
-          <div className="wrap ">
+          <div className="wrap mar">
 
             <div className="row">
-              <div className="col-sm-12 col-md-6 mar">
+              <div className="col-sm-12 col-md-6">
                 <h6>About</h6>
-                <p className="text-justify">Scanfcode.com <i>CODE WANTS TO BE SIMPLE </i> is an initiative  to help the upcoming programmers with the code. Scanfcode focuses on providing the most efficient code or snippets as the code wants to be simple. We will help programmers build up concepts in different programming languages that include C, C++, Java, HTML, CSS, Bootstrap, JavaScript, PHP, Android, SQL and Algorithm.</p>
+                <p className="text-justify">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
               </div>
               <div className="box">
               <div className="col-xs-6 col-md-auto">
@@ -42,7 +42,7 @@ export default function Footer() {
             </div>
             <hr />
           </div>
-          <div className="wrap ">
+          <div className="wrap mar">
             <div className="row">
               <div className="col-md-8 col-sm-6 col-xs-12">
                 <p className="copyright-text">Copyright © 2017 All Rights Reserved by
