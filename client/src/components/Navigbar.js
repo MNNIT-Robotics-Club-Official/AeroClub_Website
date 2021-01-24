0306deabe25103ebc2714ed6028de555d9661c59
me-abhinav-1001 <65178804+me-abhinav-1001@users.noreply.github.com>
2021-01-24 14:52:45 +0530

    minor changes

--- client/src/components/Navigbar.js
+++ client/src/components/Navigbar.js
@@ -1,4 +1,4 @@
-import { Button, Form, FormControl, Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { Button, Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import React from "react";
 import "../css/navbar.css";
  import logo from "./circle-cropped.png";
@@ -19,7 +19,7 @@ export default function Navigbar(props) {
     <>
       <Navbar sticky="top" collapseOnSelect expand="lg" variant="dark" className="style top-bottom">
       {/* <Navbar className="top-bottom" expand="lg"> */}
-        <Navbar.Brand href="#home" className="title"><a><img src={logo} style={{ height:50, marginRight:5, marginLeft:8}} />AERO CLUB MNNIT</a></Navbar.Brand>
+        <Navbar.Brand href="#home" className="title"><span><img src={logo} style={{ height:50, marginRight:5, marginLeft:8}} />AERO CLUB MNNIT</span></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
@@ -35,7 +35,7 @@ export default function Navigbar(props) {
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="https://tsaw.tech/"><Link className="pad" style={{ color: 'inherit', textDecoration: 'none' }}>Our StartUps</Link></Nav.Link>
+            <Nav.Link href="https://tsaw.tech/"><span className="pad" style={{ color: 'inherit', textDecoration: 'none'}}>Our StartUps</span></Nav.Link>
           </Nav>
           <p className="bt"><Button href="#link4" variant="light" className="mr-sm-2 my-2">Sponsorship</Button></p>
           <p className="bt"><Button href="#link5" variant="info" className="mr-sm-2 my-2">Login</Button></p>
