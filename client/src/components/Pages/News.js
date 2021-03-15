1c5b4e142e6519a0d282fc55a32c9dabd141cf09
me-abhinav-1001 <65178804+me-abhinav-1001@users.noreply.github.com>
2021-03-15 20:30:15 +0530

    completed news section

--- client/src/components/Pages/News.js
+++ client/src/components/Pages/News.js
@@ -4,6 +4,7 @@ import { Container, Jumbotron } from "react-bootstrap";
 import { animateScroll as scroll } from "react-scroll";
 import createHistory from "history/createBrowserHistory";
 import Loading from "../../Animations/Loading";
+import "../../css/news.css";
 
 export default function News() {
 
@@ -42,6 +43,91 @@ export default function News() {
           </div>
         </div>
         <Jumbotron
+          fluid
+          style={{
+            background: "white",
+            width: "100%",
+            margin: "auto",
+            paddingBottom: "1rem",
+          }}>
+          <Container>
+            <div className="panel-group" id="accordion" role="tablist" aria-multiselectable="true" activeKey={eventKey}
+              onSelect={(e) => setEventKey(e)}
+              style={{ margin: "1.5rem" }}>
+              {news.map((singleNews, i) => (
+                <div className="panel panel-default" key={singleNews.id}
+                  id={singleNews.id}
+                  style={{ padding: "0.15rem" }}
+                  data-aos="fade-up"
+                  data-aos-duration="1000">
+                  <div className="panel-heading" role="tab" id="headingOne"
+                    eventKey={singleNews.id}
+                    style={{ fontSize: "1.3rem" }}>
+                    <h4 className="panel-title">
+                      <a role="button" data-toggle="collapse" data-parent="#accordion" href={`#${i}`} aria-expanded="true" aria-controls={`${i}`} >
+                        {/* <div> */}
+                          {singleNews.title}
+                          
+                        {/* </div> */}
+                      </a>
+                    </h4>
+                  </div>
+                  <div id={`${i}`} className="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne"  eventKey={singleNews.id}>
+                    <div className="panel-body" dangerouslySetInnerHTML={{ __html: singleNews.body }}></div>
+                    <div className="panel-body">
+                    <em
+                            // className="float-right"
+                            style={{ fontSize: "small" }}
+                          >
+                            --  {new Date(
+                              singleNews.publishedAt
+                            ).toLocaleDateString()}
+                          </em>
+                    </div>
+                  </div>
+                </div>
+              ))}
+              <div className="panel panel-default">
+                <div className="panel-heading" role="tab" id="headingTwo">
+                  <h4 className="panel-title">
+                    <a className="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                      Collapsible Group Item #2
+              </a>
+                  </h4>
+                </div>
+                <div id="collapseTwo" className="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+                  <div className="panel-body">
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird
+                    on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table,
+                    raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+            </div>
+                </div>
+              </div>
+              <div className="panel panel-default">
+                <div className="panel-heading" role="tab" id="headingThree">
+                  <h4 className="panel-title">
+                    <a className="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                      Collapsible Group Item #3
+              </a>
+                  </h4>
+                </div>
+                <div id="collapseThree" className="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+                  <div className="panel-body">
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird
+                    on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table,
+                    raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+            </div>
+                </div>
+              </div>
+            </div>
+
+
+
+          </Container>
+        </Jumbotron>
+
+
+        {/* <Jumbotron
           fluid
           style={{
             background: "white",
@@ -94,7 +180,7 @@ export default function News() {
               </Accordion>
             </div>
           </Container>
-        </Jumbotron>
+        </Jumbotron> */}
       </div>
     </>
   );
