65c7064a28c53aab0def6f11f7497facff72d232
developer-ishan <ishan@mnnit.ac.in>
2021-04-08 13:49:50 +0530

    Proj Form

--- aeroclub/src/components/Pages/Dashboard/Projects/ProjForm.js
+++ aeroclub/src/components/Pages/Dashboard/Projects/ProjForm.js
@@ -3,6 +3,7 @@ import ReactQuill from "react-quill";
 import { useDispatch } from "react-redux";
 import { REACT_APP_SERVER } from "../../../../grobalVars";
 import { AddCircleOutline, RemoveCircleOutline } from "@material-ui/icons";
+import { toast } from "react-toastify";
 
 export default function ProjForm() {
   const [formData, setformData] = useState({
@@ -19,70 +20,18 @@ export default function ProjForm() {
   const [loading, setLoading] = useState(false);
   const dispatch = useDispatch();
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        setLoading(true);
-        fetch(`${REACT_APP_SERVER}/api/projects/user`, {
-          method: "post",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${localStorage.getItem("jwtToken")}`,
-          },
-          body: JSON.stringify({
-            title: formData.title,
-            description: formData.description,
-            objective: formData.objective,
-            overview: formData.overview,
-            pic: formData.pic,
-            ytID: formData.ytID,
-            compTech,
-          }),
-        })
-          .then((res) => {
-            setformData({
-              title: "",
-              overview: "",
-              description: "",
-              objective: "",
-              pic: "",
-              ytID: "",
-            });
-            setCompTech([]);
-            res.json().then((data) => {
-              dispatch({ type: "CREATE_PROJECT", payload: data });
-              setLoading(false);
-            });
-          })
-          .catch((err) => {
-            setLoading(false);
-            setformData({
-              title: "",
-              overview: "",
-              description: "",
-              objective: "",
-              pic: "",
-              ytID: "",
-            });
-            setCompTech([]);
-          });
-      }}
-    >
-      <div className="form-floating mb-3">
-        <label htmlFor="title">Project Name *</label>
-        <input
-          type="text"
-          className="form-control"
-          id="title"
-          required
-          value={formData.title}
-          onChange={(e) => {
-            setformData((prev) => ({
-              ...prev,
-              title: e.target.value,
-            }));
-          }}
-        />
+    <>
+      <div className="d-flex justify-content-center my-5">
+        <button
+          className="btn btn-danger btn-lg px-5"
+          type="button"
+          data-toggle="collapse"
+          data-target="#collapsenewproj"
+          aria-expanded="false"
+          aria-controls="collapsenewproj"
+        >
+          Create New Project
+        </button>
       </div>
       <div className="collapse my-4" id="collapsenewproj">
         <form
@@ -137,159 +86,197 @@ export default function ProjForm() {
                 setCompTech([]);
               });
           }}
-        />
-      </div>
-      <label htmlFor="overview">Overview *</label>
-      <ReactQuill
-        className="mb-3"
-        id="overview"
-        modules={{
-          toolbar: [
-            ["bold", "italic", "underline", "strike"],
-            [{ header: [1, 2, 3, 4, 5, 6, false] }],
-            [{ size: ["small", false, "large", "huge"] }],
-            [{ font: [] }],
-            [{ color: [] }, { background: [] }],
-            [{ list: "ordered" }, { list: "bullet" }],
-            [{ script: "sub" }, { script: "super" }],
-            ["blockquote", "code-block"],
-            [{ indent: "-1" }, { indent: "+1" }],
-            [{ direction: "rtl" }],
-            [{ align: [] }],
-            ["link", "image", "video"],
-            ["clean"],
-          ],
-          imageResize: {
-            displayStyles: {
-              backgroundColor: "black",
-              border: "none",
-              color: "white",
-            },
-            modules: ["Resize", "DisplaySize", "Toolbar"],
-          },
-        }}
-        value={formData.overview}
-        onChange={(e) => {
-          setformData((prev) => ({
-            ...prev,
-            overview: e,
-          }));
-        }}
-      />
-      <label htmlFor="description">Description *</label>
-      <ReactQuill
-        className="mb-3"
-        id="description"
-        modules={{
-          toolbar: [
-            ["bold", "italic", "underline", "strike"],
-            [{ header: [1, 2, 3, 4, 5, 6, false] }],
-            [{ size: ["small", false, "large", "huge"] }],
-            [{ font: [] }],
-            [{ color: [] }, { background: [] }],
-            [{ list: "ordered" }, { list: "bullet" }],
-            [{ script: "sub" }, { script: "super" }],
-            ["blockquote", "code-block"],
-            [{ indent: "-1" }, { indent: "+1" }],
-            [{ direction: "rtl" }],
-            [{ align: [] }],
-            ["link", "image", "video"],
-            ["clean"],
-          ],
-          imageResize: {
-            displayStyles: {
-              backgroundColor: "black",
-              border: "none",
-              color: "white",
-            },
-            modules: ["Resize", "DisplaySize", "Toolbar"],
-          },
-        }}
-        value={formData.description}
-        onChange={(e) => {
-          setformData((prev) => ({
-            ...prev,
-            description: e,
-          }));
-        }}
-      />
-      <div className="form-floating mb-3">
-        <label htmlFor="image">Image Link</label>
-
-        <input
-          type="url"
-          className="form-control"
-          id="image"
-          required
-          value={formData.pic}
-          onChange={(e) => {
-            setformData((prev) => ({
-              ...prev,
-              pic: e.target.value,
-            }));
-          }}
-        />
-      </div>
-      <div className="form-floating mb-3">
-        <label htmlFor="embed">
-          Youtube Embed ID ( eg : https://www.youtube.com/watch?v=
-          <b>xr5BEMrvU2A</b> ){" "}
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          id="embed"
-          required
-          value={formData.ytID}
-          onChange={(e) => {
-            setformData((prev) => ({
-              ...prev,
-              ytID: e.target.value,
-            }));
-          }}
-        />
-      </div>
-      <div className="form-floating mb-3">
-        <label htmlFor="embed">Components and Technologies Used </label>
-        {compTech.map((ct, i) => (
-          <div className="d-flex align-items-center">
+        >
+          <div className="form-floating mb-3">
+            <label htmlFor="title">Project Name *</label>
             <input
               type="text"
-              className="form-control my-2 mr-2"
-              id={`ct${i}`}
+              className="form-control"
+              id="title"
               required
-              value={ct}
+              value={formData.title}
               onChange={(e) => {
-                setCompTech(
-                  compTech.map((item, x) => (x === i ? e.target.value : item))
-                );
+                setformData((prev) => ({
+                  ...prev,
+                  title: e.target.value,
+                }));
               }}
             />
+          </div>
+          <div className="form-floating mb-3">
+            <label htmlFor="objective">Objective *</label>
+
+            <input
+              type="text"
+              className="form-control"
+              id="objective"
+              required
+              value={formData.objective}
+              onChange={(e) => {
+                setformData((prev) => ({
+                  ...prev,
+                  objective: e.target.value,
+                }));
+              }}
+            />
+          </div>
+          <label htmlFor="overview">Overview *</label>
+          <ReactQuill
+            className="mb-3"
+            id="overview"
+            modules={{
+              toolbar: [
+                ["bold", "italic", "underline", "strike"],
+                [{ header: [1, 2, 3, 4, 5, 6, false] }],
+                [{ size: ["small", false, "large", "huge"] }],
+                [{ font: [] }],
+                [{ color: [] }, { background: [] }],
+                [{ list: "ordered" }, { list: "bullet" }],
+                [{ script: "sub" }, { script: "super" }],
+                ["blockquote", "code-block"],
+                [{ indent: "-1" }, { indent: "+1" }],
+                [{ direction: "rtl" }],
+                [{ align: [] }],
+                ["link", "image", "video"],
+                ["clean"],
+              ],
+              imageResize: {
+                displayStyles: {
+                  backgroundColor: "black",
+                  border: "none",
+                  color: "white",
+                },
+                modules: ["Resize", "DisplaySize", "Toolbar"],
+              },
+            }}
+            value={formData.overview}
+            onChange={(e) => {
+              setformData((prev) => ({
+                ...prev,
+                overview: e,
+              }));
+            }}
+          />
+          <label htmlFor="description">Description *</label>
+          <ReactQuill
+            className="mb-3"
+            id="description"
+            modules={{
+              toolbar: [
+                ["bold", "italic", "underline", "strike"],
+                [{ header: [1, 2, 3, 4, 5, 6, false] }],
+                [{ size: ["small", false, "large", "huge"] }],
+                [{ font: [] }],
+                [{ color: [] }, { background: [] }],
+                [{ list: "ordered" }, { list: "bullet" }],
+                [{ script: "sub" }, { script: "super" }],
+                ["blockquote", "code-block"],
+                [{ indent: "-1" }, { indent: "+1" }],
+                [{ direction: "rtl" }],
+                [{ align: [] }],
+                ["link", "image", "video"],
+                ["clean"],
+              ],
+              imageResize: {
+                displayStyles: {
+                  backgroundColor: "black",
+                  border: "none",
+                  color: "white",
+                },
+                modules: ["Resize", "DisplaySize", "Toolbar"],
+              },
+            }}
+            value={formData.description}
+            onChange={(e) => {
+              setformData((prev) => ({
+                ...prev,
+                description: e,
+              }));
+            }}
+          />
+          <div className="form-floating mb-3">
+            <label htmlFor="image">Image Link</label>
+
+            <input
+              type="url"
+              className="form-control"
+              id="image"
+              required
+              value={formData.pic}
+              onChange={(e) => {
+                setformData((prev) => ({
+                  ...prev,
+                  pic: e.target.value,
+                }));
+              }}
+            />
+          </div>
+          <div className="form-floating mb-3">
+            <label htmlFor="embed">
+              Youtube Embed ID ( eg : https://www.youtube.com/watch?v=
+              <b>azaSaWAyQE4</b> ){" "}
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="embed"
+              required
+              value={formData.ytID}
+              onChange={(e) => {
+                setformData((prev) => ({
+                  ...prev,
+                  ytID: e.target.value,
+                }));
+              }}
+            />
+          </div>
+          <div className="form-floating mb-3">
+            <label htmlFor="embed">Components and Technologies Used </label>
+            {compTech.map((ct, i) => (
+              <div className="d-flex align-items-center">
+                <input
+                  type="text"
+                  className="form-control my-2 mr-2"
+                  id={`ct${i}`}
+                  required
+                  value={ct}
+                  onChange={(e) => {
+                    setCompTech(
+                      compTech.map((item, x) =>
+                        x === i ? e.target.value : item
+                      )
+                    );
+                  }}
+                />
+                <div
+                  className="comptech-remove d-flex"
+                  style={{ cursor: "pointer" }}
+                  onClick={() =>
+                    setCompTech((prev) =>
+                      prev.filter((item) => prev.indexOf(item) !== i)
+                    )
+                  }
+                >
+                  <RemoveCircleOutline /> Remove
+                </div>
+              </div>
+            ))}
             <div
-              className="comptech-remove d-flex"
+              className="comptech-add"
               style={{ cursor: "pointer" }}
-              onClick={() =>
-                setCompTech((prev) =>
-                  prev.filter((item) => prev.indexOf(item) !== i)
-                )
-              }
+              onClick={() => setCompTech((prev) => [...prev, ""])}
             >
-              <RemoveCircleOutline /> Remove
+              <AddCircleOutline /> Add
             </div>
           </div>
-        ))}
-        <div
-          className="comptech-add"
-          style={{ cursor: "pointer" }}
-          onClick={() => setCompTech((prev) => [...prev, ""])}
-        >
-          <AddCircleOutline /> Add
-        </div>
-      </div>
 
-      <button type="submit" className="btn btn-primary">
-        {loading ? "loading..." : "Submit"}
-      </button>
-    </form>
+          <div className="d-flex justify-content-center">
+            <button type="submit" className="btn btn-primary">
+              {loading ? "loading..." : "Submit"}
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
   );
 }
