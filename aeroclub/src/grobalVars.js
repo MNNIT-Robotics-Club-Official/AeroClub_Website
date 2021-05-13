edd2ff1e913d0e3c71f28d710732fc2baf64a641
Parnab Ghosh <parnab0410@gmail.com>
2021-05-14 01:27:17 +0530

    changes made

--- aeroclub/src/grobalVars.js
+++ aeroclub/src/grobalVars.js
@@ -1,4 +1,3 @@
 const REACT_APP_BASE_TITLE = "AeroClub MNNIT";
-const REACT_APP_SERVER = "http://localhost:9501";
-// const REACT_APP_SERVER = "https://aeroclub.roboticsclubmnnit.website";
+const REACT_APP_SERVER = "";
 export { REACT_APP_BASE_TITLE, REACT_APP_SERVER };
