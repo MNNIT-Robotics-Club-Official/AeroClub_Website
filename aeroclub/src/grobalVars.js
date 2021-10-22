0e13855099a2d8f586d1b06b402a4b4ced8c9790
Parnab Ghosh <parnab0410@gmail.com>
2021-10-22 20:45:49 +0530

    some changes in alumni section

--- aeroclub/src/grobalVars.js
+++ aeroclub/src/grobalVars.js
@@ -1,4 +1,4 @@
 const REACT_APP_BASE_TITLE = "AeroClub MNNIT";
-// const REACT_APP_SERVER = "";
-const REACT_APP_SERVER = "http://localhost:9501";
+// const REACT_APP_SERVER = "http://localhost:9501";
+const REACT_APP_SERVER = "";
 export { REACT_APP_BASE_TITLE, REACT_APP_SERVER };
